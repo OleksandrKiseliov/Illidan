1aa135241c246e3c39fb69d9567b46de3d69ae06
OleksandrKiseliov <sashakiseliov20151@gmail.com>
2023-09-21 19:09:26 +0300

    lab 1

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const { gulpfile } = require("gulp-cli/lib/shared/cli-options");
+
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
+  gulp
